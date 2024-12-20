3401c9a55bc0666935a61e18e2e4bfea2b30ca57
Roberto Torres <rocatoro18@outlook.com>
2024-12-19 21:51:52 -0700

    .

---
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  templateUrl: './change-detection.component.html',
+  styles: ``
+})
+export class ChangeDetectionComponent {
+
+}
---
+++ src/app/dashboard/pages/defer-option/defer-option.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  templateUrl: './defer-option.component.html',
+  styles: ``
+})
+export class DeferOptionComponent {
+
+}
