0ed04bc4ec034c000f7e2dc492e687853cb1cca7
Roberto Torres <ingenieria5@lopez-fernandez.com>
2024-12-23 10:35:50 -0700

    .

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { SidemenuComponent } from '../shared/sidemenu/sidemenu.component';
 
 @Component({
-  imports: [RouterModule],
+  imports: [RouterModule, SidemenuComponent],
   templateUrl: './dashboard.component.html',
   styles: ``
 })
