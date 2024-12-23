4a9dde5f39cc38139d644f55d9ebf9f9b716475b
Roberto Torres <ingenieria5@lopez-fernandez.com>
2024-12-23 13:07:16 -0700

    .

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { routes } from '../../app.routes';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidemenu',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './sidemenu.component.html',
   styleUrl: './sidemenu.component.css'
 })
