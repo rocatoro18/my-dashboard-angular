8a0942be571ff116cc7cba2f9ab1802e3cb43b2b
Roberto Torres <ingenieria5@lopez-fernandez.com>
2024-12-26 17:56:45 -0700

    LPGVAC

--- src/app/dashboard/pages/users/users.component.ts
+++ src/app/dashboard/pages/users/users.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { UsersService } from '../../../services/users.service';
 
+
 @Component({
   imports: [],
   templateUrl: './users.component.html',
